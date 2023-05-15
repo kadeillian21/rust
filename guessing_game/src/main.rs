464bfb2c093002706057099a5429659b0fb81b49
Kade Illian <kade.illian@biproxi.com>
2023-05-15 08:13:08 -0600

    added a loop that only breaks when the user guesses the correct number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,19 +8,23 @@ fn main() {
     println!("Guess the number!");
     println!("Pleas input your guess.");
 
-    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
-    let mut guess = String::new();
+    loop {
+        let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-    println!("The secret number was: {}", secret_number);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+        println!("The secret number was: {}", secret_number);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
