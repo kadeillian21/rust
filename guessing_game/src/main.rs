9623552900dc8187f02eb563eb115e07bb41f71b
Kade Illian <kade.illian@biproxi.com>
2023-05-15 08:25:43 -0600

    finished chapter 2 and completed the rust random number guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,18 +6,21 @@ use std::io;
 // HELLLOOOOOOO RUST COMMENTSS!!!
 fn main() {
     println!("Guess the number!");
-    println!("Pleas input your guess.");
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
 
     loop {
-        let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+        println!("Please input your guess.");
+
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
-        println!("The secret number was: {}", secret_number);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
