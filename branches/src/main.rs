2a4553205d3c2d034fecf74732e70cf16a385495
Kade Illian <kade.illian@biproxi.com>
2023-05-27 22:52:33 -0600

    loops

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let number: u8 = 7;
+
+    if number < 5 {
+        println!("Condition was true");
+    } else {
+        println!("Condition was false");
+    }
+
+    // ? Else If
+
+    let mango: &str = "This is a mango moment";
+
+    if mango == "This is a mango moment" {
+        println!("Wow, this is epic");
+    } else if mango == "pie" {
+        println!("Pie moment");
+    } else {
+        println!("This input not valid");
+    }
+
+    // ! Using "if" in a "let" statement
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {}", number);
 }
