18842f436ad99cfb2fbd9846ecd51ff5a37dc50e
Kade Illian <kade.illian@biproxi.com>
2023-05-27 22:36:52 -0600

    remove conditiaonals for branches, following the Rust book

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/kadeillian/personal-code/rust/branches/target/debug/branches: /Users/kadeillian/personal-code/rust/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-8edfee23a528a813.d
@@ -0,0 +1,5 @@
+/Users/kadeillian/personal-code/rust/branches/target/debug/deps/branches-8edfee23a528a813: src/main.rs
+
+/Users/kadeillian/personal-code/rust/branches/target/debug/deps/branches-8edfee23a528a813.d: src/main.rs
+
+src/main.rs:
