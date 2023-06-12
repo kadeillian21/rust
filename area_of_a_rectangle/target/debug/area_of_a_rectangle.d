7b703897076774b2002848a4da452c01dc92bc04
Kade Illian <kade.illian@biproxi.com>
2023-06-11 22:25:52 -0600

    tuple structs

---
+++ area_of_a_rectangle/target/debug/area_of_a_rectangle.d
@@ -0,0 +1,1 @@
+/Users/kadeillian/personal-code/rust/area_of_a_rectangle/target/debug/area_of_a_rectangle: /Users/kadeillian/personal-code/rust/area_of_a_rectangle/src/main.rs
---
+++ area_of_a_rectangle/target/debug/deps/area_of_a_rectangle-98a93bd35d32f1db.d
@@ -0,0 +1,5 @@
+/Users/kadeillian/personal-code/rust/area_of_a_rectangle/target/debug/deps/area_of_a_rectangle-98a93bd35d32f1db: src/main.rs
+
+/Users/kadeillian/personal-code/rust/area_of_a_rectangle/target/debug/deps/area_of_a_rectangle-98a93bd35d32f1db.d: src/main.rs
+
+src/main.rs:
