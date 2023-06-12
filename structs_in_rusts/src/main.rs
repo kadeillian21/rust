fa2321bea3ad57afd89bbba744321794770d5fea
Kade Illian <kade.illian@biproxi.com>
2023-06-11 22:18:52 -0600

    structs

--- structs_in_rusts/src/main.rs
+++ structs_in_rusts/src/main.rs
@@ -5,7 +5,16 @@ struct User {
     sign_in_count: u64,
 }
 
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
 fn main() {
+    // this is the lame way to instantiate an object in Rust... let's get a bit more functional with the build_user function call
     let mut user1 = User {
         active: true,
         username: String::from("someusername123"),
@@ -13,7 +22,20 @@ fn main() {
         sign_in_count: 1,
     };
 
+    let user2 = build_user(
+        String::from("email@params.com"),
+        String::from("hiIAmAUsername"),
+    );
     user1.email = String::from("anotheremail@example.com");
 
+    let user3 = User {
+        email: String::from("ThisIsHowYouUpdateAnObjectInRust@gmail.com"),
+        ..user2
+    };
+
+    println!("{}", user1.active);
+    println!("{}", user1.username);
     println!("{}", user1.email);
+    println!("{}", user1.sign_in_count);
+    println!("{}", user3.email);
 }
